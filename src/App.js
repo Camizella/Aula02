c8240a48d70a11ab26050cde2b0e37493d5318f2
Camizella <camilla_padilha@yahoo.com.br>
2024-09-03 22:11:27 -0300

    aula02

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import PrimeiroComponente from './Components/PrimeiroComponente';
+import SegundoComponente from './Components/SegundoComponent';
+function App() {
+  return (
+    <>
+    <PrimeiroComponente/>
+    <SegundoComponente/>
+    </>
+
+    
+    );
+}
+
+export default App;
---
+++ src/Components/PrimeiroComponente/index.js
@@ -0,0 +1,24 @@
+import"./primeiro.css";
+import { useState } from "react";
+
+export default function PrimeiroComponente(){
+    const [num, setNum] = useState(14);
+
+    function increment(){
+        setNum(a => a + 1);
+    }
+    function decrement(){
+        setNum(a => a - 1);
+    }
+    
+    return(
+        <p className = "paragrafo"> 
+        <button onClick={decrement}> - </button>
+
+        {num}
+
+        <button onClick={increment}> + </button>
+        Meu primeiro componente
+        </p>
+    )
+}
